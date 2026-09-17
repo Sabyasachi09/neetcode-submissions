0aa12e0d2bfd8d42395d4f77934853772bff5c10
Sabyasachi Choubey <sab.choubey@hotmail.com>
2026-09-17 20:45:07 +0530

    Add: is-anagram - submission-31

---
+++ Data Structures & Algorithms/is-anagram/submission-31.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] charArrayS = s.toCharArray(); // O(n)
+        char[] charArrayT = t.toCharArray(); // O(n)
+        Arrays.sort(charArrayS); // O(nLogn)
+        Arrays.sort(charArrayT); // O(nLogn)
+        if(Arrays.equals(charArrayS, charArrayT)){ // O(n)
+            return true;
+        }
+        return false;
+    }
+}
+// Time Complexity: O(n) + O(n) + O(nLogn) + O(nLogn) + O(n) + O(n) + O(n) = O(nLogn)
